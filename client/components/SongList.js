aacff4d7cf2cc480f8da93055b3d87d5ef56566b
Adam Marczyk <adam.marczyk90@gmail.com>
2018-07-19 10:53:15 +0200

    add query

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import gql from 'graphql-tag';
 
 class SongList extends React.Component {
   render() {
     return <div>SongList</div>;
   }
 }
+
+const query = gql`
+  {
+    songs {
+      title
+    }
+  }
+`;
 
 export default SongList;
